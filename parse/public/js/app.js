2cf45bb0c8be0b18d4ac9d5e2e268bcabd7431f2
jeanettepranin <jeanettepranin@gmail.com>
2016-03-24 23:16:00 -0700

    transfer from separate js file to app.js

--- parse/public/js/app.js
+++ parse/public/js/app.js
@@ -25,11 +25,13 @@ aLevelApp.config(['$routeProvider',
       });
 }]);
  
+
 aLevelApp.controller('LoginController', function($scope) {
      
      
 });
 
+ 
 
 aLevelApp.factory("coursesService", function($rootScope, $q){
 
@@ -51,8 +53,8 @@ aLevelApp.factory("coursesService", function($rootScope, $q){
           $rootScope.$apply();
         },
         error: function(error){
-          console.log("Error in loading courses: " + error);
-          deferred.reject(results);
+          console.log("Error in loading courses: " + error.message);
+          deferred.reject(error);
           //update the view!!
           $rootScope.$apply();
         }
@@ -85,8 +87,8 @@ aLevelApp.factory("majorsService", function($rootScope, $q){
           $rootScope.$apply();
         },
         error: function(error){
-          console.log("Error in loading majors: " + error);
-          deferred.reject(results);
+          console.log("Error in loading majors: " + error.message);
+          deferred.reject(error);
           //update the view!!
           $rootScope.$apply();
         }
@@ -99,7 +101,7 @@ aLevelApp.factory("majorsService", function($rootScope, $q){
 
 });
 
-aLevelApp.controller('SignUpController', function($scope, coursesService, majorsService) {
+aLevelApp.controller('SignUpController', function($scope, $location, coursesService, majorsService) {
 
   $scope.courses = [];
 
@@ -158,7 +160,8 @@ aLevelApp.controller('SignUpController', function($scope, coursesService, majors
       lastName = capitalizeString($scope.lastName);
     }
     email = $scope.email;
-    if(typeof email == "undefined"){
+    //also check if @ and . symbols are in the email
+    if(typeof email == "undefined" || email.indexOf("@") == -1 || email.indexOf(".") == -1){
       needsFillingOut.push("Email");
     }
     password = $scope.password;
@@ -169,6 +172,7 @@ aLevelApp.controller('SignUpController', function($scope, coursesService, majors
     if(typeof majors == "undefined"){
       needsFillingOut.push("Major");
     }
+    //also check that class year is a four digit number
     classYear = $scope.classYear;
     if(typeof classYear == "undefined" || classYear.toString().length != 4 ){
       needsFillingOut.push("Class Year");
@@ -185,12 +189,15 @@ aLevelApp.controller('SignUpController', function($scope, coursesService, majors
     if(typeof phone == "undefined"){
       needsFillingOut.push("Phone");
     }
+    else {
+      phone = parseInt($scope.phone);
+    }
     type = $scope.userType;
     if(typeof type == "undefined"){
       needsFillingOut.push("Type");
     }
 
-    //if something wasn't filled in correctly
+    //if something wasn't filled out correctly
     if(needsFillingOut.length > 0){
       var html = "The following fields have not been entered correctly. Please fill them out and try again:<br>";
       for(var i = 0; i < needsFillingOut.length; i++){
@@ -205,21 +212,209 @@ aLevelApp.controller('SignUpController', function($scope, coursesService, majors
     }
 
     //otherwise, there's no error
+    //sign this person up!
     else {
+      //clear error div
       $("#error").html("");
+
+      //make new Parse User
+      var newUser = new Parse.User();
+      
+      //set new person's username to be email for now
+      newUser.set("username", email);
+      newUser.set("password", password);
+      newUser.set("email", email);
+      newUser.set("type", type);
+      newUser.set("firstName", firstName);
+      newUser.set("lastName", lastName);
+      newUser.set("majors", majors);
+      newUser.set("classYear", classYear);
+      newUser.set("subjects", courses);
+      newUser.set("division", division);
+      newUser.set("phoneNumber", phone);
+
+      newUser.signUp(null, {
+        success: function(user) {
+          // Hooray! new user signed up successfully
+
+          console.log("Signing up of new user successful!");
+          if(type == "tutor"){
+            $location.path("tutordashboard");
+          }
+          else if(type == "student"){
+            $location.path("login");
+          }
+          $scope.$apply();
+
+
+        },
+        error: function(user, error){
+          // Show the error message somewhere and let the user try again.
+          alert("Error in signing up new user: " + error.code + " " + error.message);
+        }
+
+      });
+
+
     }
   }
 
 });
 
 
+aLevelApp.controller('TutorDashboardController', function($scope, $location) {
+     var currentUser = Parse.User.current();
+
+     $scope.messages = [];
+     $scope.filters = [];
+
+     if(!currentUser){
+        //no one is signed in, go to login page
+        $location.path("login");
+        $scope.$apply();
+     }
+     else{
+      /////////////////LOAD MESSAGES//////////////////////
+      var messageClass = Parse.Object.extend("Message");
+      var messageQuery = new Parse.Query(messageClass);
+
+      //order by date created (older ones should go first)
+      messageQuery.ascending("createdAt");
+
+      messageQuery.find({
+        success: function(results){
+          console.log("Messages successfully retrieved from database");
+          //hide loading gif
+          $("#messageLoad").css("display", "none");
+
+          //if there are no messages
+          if(results.length == 0){
+            $("#messages").html("<p>No messages found</p>");
+          }
+          //otherwise, prepare the messages
+          else {
+
+            for(var i = 0; i < results.length; i++){
+              var obj = {};
+              var createdAt = new Date();
+              createdAt = results[i]["createdAt"];
+
+              obj["header"] = results[i].get("header");
+              obj["createdAt"] = createdAt.toDateString() + " " + formatAMPM(createdAt);
+              obj["content"] = results[i].get("content");
+
+              $scope.messages.push(obj);
+              
+            }
+            $scope.$apply();
+
+          }
+        },
+        error: function(error){
+          console.log("Error in startLoadMessages(): " + error);
+          $("#messages").html("<p>No messages found, database error</p>");
+        }
+      });
+      ////////////LOAD FILTERS//////////////////
+      var subjectClass = Parse.Object.extend("Subject");
+      var subjectQuery = new Parse.Query(subjectClass);
+
+      subjectQuery.ascending("Department,Course_Number");
+      
+
+      subjectQuery.find({
+        success: function(results){
+          console.log("Filters successfully retrieved from database");
+
+          //delete loading filter
+          $("#filterLoad").css("display", "none");
 
 
+          if(results.length == 0){
+            $("#filters").html("<p>No filters found</p>");
+          }
+          else {
+            for(var i = 0; i < results.length; i++){
+              var obj = {};
+              var dept = results[i].get("Department");
+              var courseNum = results[i].get("Course_Number");
 
-aLevelApp.controller('TutorDashboardController', function($scope) {
-     
+              obj["id"] = dept + "_" + courseNum;
+              obj["name"] = dept + " " + courseNum;
+              $scope.filters.push(obj);
+            }
+            $scope.$apply();
+          }
+        },
+        error: function(error){
+          console.log("Error in startLoadFilters(): " + error);
+          $("#filters").html("<p>No filters found</p>");
+        }
+      });
+    }
+
+    $scope.filterMessages = function(){
+      var checkboxes = document.getElementsByClassName("checkboxes");
+      var filterList = [];
+      for(var i = 0; i < checkboxes.length; i++){
+        if(checkboxes[i].checked){
+          filterList.push(checkboxes[i].id);
+        }
+      }
+      //nothing is checked, we want all possible options
+      if(filterList.length == 0){
+        for(var i = 0; i < checkboxes.length; i++){
+          filterList.push(checkboxes[i].id);
+        }
+      }
+      //apply the checked filters
+      $scope.applyFilters(filterList);
+      
+    };
+
+    $scope.applyFilters = function(filterList){
+
+      //we are going to query the messages to see which ones have the selected subjects
+      var messageQuery = new Parse.Query(Parse.Object.extend("Message"));
+      messageQuery.containedIn("subjects", filterList);
+      messageQuery.ascending("createdAt");
+
+      messageQuery.find({
+        success: function(results){
+          //if we find messages
+          console.log("Messages with filters successfully retrieved from database");
+          //no messages
+          if(results.length == 0){
+            $("#messages").html("<p>No messages with such filters available</p>");
+          }
+          else {
+            $scope.messages = [];
+            for(var i = 0; i < results.length; i++){
+              var obj = {};
+              var createdAt = new Date();
+              createdAt = results[i]["createdAt"];
+
+              obj["header"] = results[i].get("header");
+              obj["createdAt"] = createdAt.toDateString() + " " + formatAMPM(createdAt);
+              obj["content"] = results[i].get("content");
+
+              $scope.messages.push(obj);
+            }
+            $scope.$apply();
+          }
+        },
+        error: function(error){
+          //database error
+          console.log("Error in applyFilters(): " + error);
+          $("#messages").html("<p>No messages with such filters available, database error</p>");
+        }
+      }); 
+    };
+
+
      
 });
+
 
 //helper function to make input more readable
 function capitalizeString(str){
@@ -228,3 +423,18 @@ function capitalizeString(str){
 
   return first.toUpperCase() + rest.toLowerCase();
 }
+
+//helper function to convert to AM/PM in loadMessagesHTML()
+function formatAMPM(date) {
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  var ampm = hours >= 12 ? 'pm' : 'am';
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+  minutes = minutes < 10 ? '0'+minutes : minutes;
+  var strTime = hours + ':' + minutes + ' ' + ampm;
+  return strTime;
+}
+
+
+
